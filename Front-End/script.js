e4e880448640b70c440549cb6ddc62a6821a3b21
Rudra-CSER <97387423+Rudra-CSER@users.noreply.github.com>
2025-05-22 15:34:35 +0530

    api call added and basic backend for search bar on click added

--- Front-End/script.js
+++ Front-End/script.js
@@ -1,9 +1,36 @@
 let weather ={
     apiKey: "723ce25af142d6f3eadd721bc0a83472",
-    fetchWeather: function () {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=kolkata&units=metric&appid=723ce25af142d6f3eadd721bc0a83472")
+    fetchWeather: function (city) {
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
         .then((response) => response.json())
-        .then((data) => console.log(data));
-        },
+        .then((data) => {
+            console.log(data);              // ✅ still logs
+            this.displayWeather(data)});    // ✅ returns the data 
+    },
+
+ displayWeather: function (data) {
+    const { name } = data;
+    const { icon,description } = data.weather[0];
+    const {temp , humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name , description, temp, humidity, speed);
+
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+
+}
+ 
+    
 };
+
+document.querySelector(".search button").addEventListener("click", () => {
+    const city = document.querySelector(".search-bar").value.trim();
+    if(city){
+        weather.fetchWeather(city);
+    }
+
+});
+
